3ea99e182f334d6145cc4712cf8f8a1353e259d6
Henrique-kriguer <henriquekriguer@gmail.com>
2025-06-17 17:08:32 -0300

    metodo capturar campo formulario done

--- ListaCompras/src/app/app.ts
+++ ListaCompras/src/app/app.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'listaCompras';
+  item: string = '';
+
+  adicionarItem() {
+    console.log('Item adicionado:', this.item);
+    this.item = ''; // Limpa o campo de entrada após adicionar o item
+  }
 }
