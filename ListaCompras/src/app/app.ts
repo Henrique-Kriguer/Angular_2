7ed740a2d4470e19a256d47acb2dbaa9e1517f63
Henrique-kriguer <henriquekriguer@gmail.com>
2025-06-18 16:54:30 -0300

    metodo inserir item na lista renderizado e riscar item comprado

--- ListaCompras/src/app/app.ts
+++ ListaCompras/src/app/app.ts
@@ -1,20 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista'
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule],
+  imports: [RouterOutlet,FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'listaCompras';
-  item: string = '';
 
-  adicionarItem() {
-    console.log('Item adicionado:', this.item);
+  item: string = '';
+  lista: ItemLista[] = [];
+
+  adicionarItem() { 
+    let itemlista = new ItemLista();
+    itemlista.nome = this.item;
+    itemlista.id = this.lista.length + 1; // Atribui um ID baseado no tamanho atual da lista
+    this.lista.push(itemlista);
     this.item = ''; // Limpa o campo de entrada após adicionar o item
+
+}
+  riscarItem(itemlista: ItemLista) {
+    itemlista.comprado = !itemlista.comprado; // Alterna o estado de comprado (true/false)
   }
 }
