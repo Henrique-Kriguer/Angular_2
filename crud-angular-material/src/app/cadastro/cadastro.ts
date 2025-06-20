3f2472b9b903a9a8a4809279395143ee166a7ecd
Henrique-kriguer <henriquekriguer@gmail.com>
2025-06-20 16:38:23 -0300

    campos e btn formulario cadastro cliente

--- crud-angular-material/src/app/cadastro/cadastro.ts
+++ crud-angular-material/src/app/cadastro/cadastro.ts
@@ -4,10 +4,20 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from "@angular/material/icon"; 
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [FlexLayoutModule, MatCardModule, FormsModule, MatInputModule, MatFormFieldModule],
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss'
 })
