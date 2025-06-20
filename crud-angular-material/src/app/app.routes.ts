20b4592c554d211926b67be6c7fd7b4bf7df95c7
Henrique-kriguer <henriquekriguer@gmail.com>
2025-06-20 11:16:46 -0300

    Projeto crud-angular-material components cadastro consulta

--- crud-angular-material/src/app/app.routes.ts
+++ crud-angular-material/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Cadastro } from './cadastro/cadastro';
+import { Consulta } from './consulta/consulta';
+export const routes: Routes = [
+    { path: 'cadastro', component: Cadastro },
+    { path: 'consulta', component: Consulta}
+];
---
+++ crud-angular-material/src/app/cadastro/cadastro.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [MatToolbar],
+  templateUrl: './cadastro.html',
+  styleUrl: './cadastro.scss'
+})
+export class Cadastro {
+
+}
