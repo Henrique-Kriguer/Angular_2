0d4dbdb9e208910ecfda5c8f51bf1f0aceb392cf
Henrique-kriguer <henriquekriguer@gmail.com>
2025-06-26 15:38:29 -0300

    implantação serviço armazenar form no localStorage

--- crudCliente/src/app/customer.ts
+++ crudCliente/src/app/customer.ts
@@ -6,9 +6,24 @@ import { Cliente } from './cadastro/cliente';
 })
 export class Customer {
 
+  static REPO_CLIENTES = "_CLIENTES";
+  
   constructor() { }
     salvar(cliente: Cliente) {
-      console.log('Dados Cliente: ', cliente);
+      const storage = this.obterStorage();
+      storage.push(cliente);
+      localStorage.setItem(Customer.REPO_CLIENTES, JSON.stringify(storage));
     }
+
+    obterStorage() : Cliente[] {
+      const clientesString = localStorage.getItem(Customer.REPO_CLIENTES);
+      if (clientesString) {
+        const clientes: Cliente[] = JSON.parse(clientesString);
+        return clientes;
+      }
+
+      const clientes: Cliente[] = [];
+      localStorage.setItem(Customer.REPO_CLIENTES, JSON.stringify(clientes));
+      return clientes;    }
   
 }
