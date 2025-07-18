1c39bc50a4014f2c6d4addb9c10a1c68800f2bce
Henrique-kriguer <henriquekriguer@gmail.com>
2025-07-18 14:53:08 -0300

    implementando modulo e componente categoria

--- travel-app/src/app/template/template-routing-module.ts
+++ travel-app/src/app/template/template-routing-module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Layout } from './layout/layout';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'paginas',
+    component: Layout,
+    children: [
+      {
+        path: 'categorias',
+        loadChildren: () => import('../categorias/categorias-module').then( m => m.CategoriasModule)
+      }
+    ]
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
