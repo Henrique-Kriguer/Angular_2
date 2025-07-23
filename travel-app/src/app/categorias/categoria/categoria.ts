86785783c31d5d3ea98aaefc00583f5dd734b5d7
Henrique-kriguer <henriquekriguer@gmail.com>
2025-07-23 16:21:11 -0300

    aplicando reatividade no formulario de cadastro

--- travel-app/src/app/categorias/categoria/categoria.ts
+++ travel-app/src/app/categorias/categoria/categoria.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{ FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-categoria',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './categoria.scss'
 })
 export class Categoria {
+  camposForm: FormGroup;
 
+  constructor(){
+    this.camposForm = new FormGroup({
+        nome: new FormControl('', Validators.required ),
+        descricao: new FormControl('', Validators.required)
+    });
+  }
+
+  salvar(){
+    this.camposForm.markAllAsTouched();
+    console.log('valores digitados: ', this.camposForm.value)
+    console.log('Esta valido?', this.camposForm.valid)
+  }
+
+  isCampoInvalido(nomeCampo: string) : boolean{
+    const campo = this.camposForm.get(nomeCampo)
+    return (campo?.invalid && campo.touched && campo.errors?.['required']) || false;
+  }
 }
