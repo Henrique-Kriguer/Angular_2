144a06e46e4b79b580962faefe15d68b87c0c1db
Henrique-kriguer <henriquekriguer@gmail.com>
2025-07-31 16:37:00 -0300

    configurando a página de lugares - formulario

--- travel-app/src/app/lugares/lugar/lugar.ts
+++ travel-app/src/app/lugares/lugar/lugar.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { Categoria } from '../../categorias/categoria';
 @Component({
   selector: 'app-lugar',
   standalone: false,
@@ -8,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class Lugar {
 
+  camposForm: FormGroup;
+  categoria: Categoria[] = [];
+
+  constructor(){
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      localizacao: new FormControl('', Validators.required),
+      urlFoto: new FormControl('', Validators.required),
+      avaliacao: new FormControl('', Validators.required)
+    });
+  }
+
+  salvar(){
+    console.log("valores: ", this.camposForm.value);
+  }
 }
