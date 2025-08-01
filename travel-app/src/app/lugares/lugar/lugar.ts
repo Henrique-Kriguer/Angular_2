ff79a78b74680d4fc621c68126facf1b258e64b1
Henrique-kriguer <henriquekriguer@gmail.com>
2025-08-01 15:01:12 -0300

    implementando metodo salvar() p/ categorias no formulario

--- travel-app/src/app/lugares/lugar/lugar.ts
+++ travel-app/src/app/lugares/lugar/lugar.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { Categoria } from '../../categorias/categoria';
 import { CategoriaService } from '../../categorias/categoria-service';
+import { LugarService } from '../lugar-service';
 @Component({
   selector: 'app-lugar',
   standalone: false,
@@ -13,7 +14,9 @@ export class Lugar implements OnInit{
   camposForm: FormGroup;
   categorias: Categoria[] = [];
 
-  constructor(private categoriaService: CategoriaService){
+  constructor(private categoriaService: CategoriaService,
+              private service: LugarService
+  ){
     this.camposForm = new FormGroup({
       nome: new FormControl('', Validators.required),
       categoria: new FormControl('', Validators.required),
@@ -24,15 +27,20 @@ export class Lugar implements OnInit{
   }
 
   ngOnInit(): void {
-    console.log("Carregando as categorias da api")
+    
     this.categoriaService.obterTodas().subscribe({
       next: (listaCategorias) => {
-        console.log("Recebendo as categorias: ", listaCategorias)
         this.categorias = listaCategorias
       }
     })
   }
   salvar(){
-    console.log("valores: ", this.camposForm.value);
+    this.service.salvar(this.camposForm.value)
+      .subscribe({
+        next: (lugar) => {console.log('Cadastrado com sucesso!', lugar),
+                          this.camposForm.reset();
+        },
+        error: (error) => console.error('ocorreu um erro!', error)
+      });
   }
 }
