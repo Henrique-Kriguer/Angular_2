a443d0270ec671c1390240a22781779514b650e4
Henrique-kriguer <henriquekriguer@gmail.com>
2025-08-04 17:34:39 -0300

    criando a página de galeria - implementando cards

--- travel-app/src/app/galeria/galeria/galeria.ts
+++ travel-app/src/app/galeria/galeria/galeria.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Lugar } from '../../lugares/lugar';
+import { Categoria } from '../../categorias/categoria';
+import { LugarService } from '../../lugares/lugar-service';
+import { CategoriaService } from '../../categorias/categoria-service';
 @Component({
   selector: 'app-galeria',
   standalone: false,
   templateUrl: './galeria.html',
   styleUrl: './galeria.scss'
 })
-export class Galeria {
+export class Galeria implements OnInit{
+  lugares: Lugar[] = [];
+  categoriasFiltro: Categoria[] = [];
 
+  constructor(
+    private lugarService: LugarService,
+    private categoriaService: CategoriaService
+  ){}
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodas()
+    .subscribe(categorias => this.categoriasFiltro = categorias);
+
+    this.lugarService.obterTodos()
+    .subscribe(lugaresResposta => this.lugares = lugaresResposta)
+  }
 }
