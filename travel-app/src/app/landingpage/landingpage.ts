dd92e1ef28268563c14917271749b70eb7ca93f7
Henrique-kriguer <henriquekriguer@gmail.com>
2025-08-11 16:50:41 -0300

    implementando pagina inicial

--- travel-app/src/app/landingpage/landingpage.ts
+++ travel-app/src/app/landingpage/landingpage.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Profile } from './profile.model';
+import { Router } from '@angular/router';
+import { NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-landingpage',
@@ -8,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class Landingpage {
 
+  profile: Profile | undefined;
+
+  constructor(private router: Router){
+
+  }
+
+  navegar(){
+    this.router.navigate(['/paginas/galeria']);
+  }
+
+  logarComGoogle(){
+
+  }
+
+  isLoggedIn(): boolean{
+    return !!this.profile;
+  }
 }
